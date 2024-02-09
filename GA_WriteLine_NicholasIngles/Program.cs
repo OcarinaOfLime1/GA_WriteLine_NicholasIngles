954fb75813d90f2ecddf3dc44f8588f36584943b
Nicholas Ingles <nringles@student.rtc.edu>
2024-02-08 23:22:06 -0800

    Added Author Quotes

--- GA_WriteLine_NicholasIngles/Program.cs
+++ GA_WriteLine_NicholasIngles/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.Write("Edsger W.Dijkstra: ");
+Console.WriteLine("Computer science is no more about computers than astronomy is about telescopes.");
+
+Console.Write("Alan Kay: ");
+Console.WriteLine("The best way to predict the future is to invent it.");
+
+Console.Write("Grace Hopper: ");
+Console.WriteLine("The most damaging phrase in the language is, We’ve always done it this way.");
+
+Console.Write("Terry Pratchett (Discworld): ");
+Console.WriteLine("It's not worth doing something unless someone, somewhere, would much rather you weren't doing it.");
